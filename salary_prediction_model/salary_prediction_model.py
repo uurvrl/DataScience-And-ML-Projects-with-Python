8d169c2fef3496c41174ab8d9a6c79f50ac83429
uurvrl <107815698+uurvrl@users.noreply.github.com>
2022-08-04 03:57:52 +0300

    Add files via upload

---
+++ salary_prediction_model/salary_prediction_model.py
@@ -0,0 +1,368 @@
+# Dataset story
+
+# This dataset was originally taken from the StatLib library at Carnegie Mellon University.
+# The dataset is part of the data used in the 1988 ASA Graphics Section Poster Session.
+# Salary data originally taken from Sports Illustrated, April 20, 1987.
+# 1986 and career statistics, published by Collier Books, Macmillan Publishing Company, New York
+# Obtained from the 1987 Baseball Encyclopedia Update.
+
+
+# AtBat: Number of hits with a baseball bat during the 1986-1987 season
+# Hits: the number of hits in the 1986-1987 season
+# HmRun: Most valuable hits in the 1986-1987 season
+# Runs: The points he earned for his team in the 1986-1987 season
+# RBI: The number of players a batter had jogged when he hit
+# Walks: Number of mistakes made by the opposing player
+# Years: Player's playing time in major league (years)
+# CAtBat: The number of times the player hits the ball during his career
+# CHits: The number of hits the player has made throughout his career
+# CHmRun: The player's most valuable number during his career
+# CRuns: The number of points the player has earned for his team during his career
+# CRBI: The number of players the player has made during his career
+# CWalks: The number of mistakes the player has made to the opposing player during his career
+# League: A factor with levels A and N, showing the league in which the player played until the end of the season
+# Division: a factor with levels E and W, showing the position played by the player at the end of 1986
+# PutOuts: Helping your teammate in-game
+# Assits: The number of assists the player made in the 1986-1987 season
+# Errors: the number of errors of the player in the 1986-1987 season
+# Salary: The salary of the player in the 1986-1987 season (over thousand)
+# NewLeague: a factor with levels A and N indicating the league of the player at the beginning of the 1987 season
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+import warnings
+warnings.simplefilter(action="ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+df = pd.read_csv("salary_prediction_model/hitters.csv")
+
+############################
+# FIRST LOOK AT THE DATA
+############################
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+check_df(df)
+
+############################
+# DEFINING THE COLUMN VALUES
+############################
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtype == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtype != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtype == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtype != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+############################
+# ANALYSIS OF NUMERICAL VALUES
+############################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+############################
+# ANALYSIS OF CATEGORICAL VALUES
+############################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+for col in cat_cols:
+    cat_summary(df, col, plot=True)
+
+############################
+# OUTLIERS
+############################
+
+def outlier_thresholds(dataframe, col_name, q1=0.10, q3=0.90):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+for col in num_cols:
+    print(col)
+    print(outlier_thresholds(df, col))
+
+def check_outlier(dataframe, col_name, q1=.25, q3=.75):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name, q1, q3)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+############################
+# SUPRESSING OUTLIERS
+############################
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+for col in num_cols:
+    if check_outlier(df, col) == True:
+        replace_with_thresholds(df, col)
+
+############################
+# CORRELATION
+############################
+
+def correlation_matrix(df, cols):
+    fig = plt.gcf()
+    fig.set_size_inches(10, 8)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    fig = sns.heatmap(df[cols].corr(), annot=True, linewidths=0.5, annot_kws={'size': 12}, linecolor='w', cmap='RdBu')
+    plt.show(block=True)
+
+
+correlation_matrix(df, num_cols)
+
+############################
+# MISSING VALUES
+############################
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+
+    if na_name:
+        return na_columns
+
+missing_values_table(df)
+
+############################
+# REMOVING THE MISSING VALUES FROM THE TABLE
+############################
+
+# Because missing values are from the target value, It is better to remove missing values rather than other options.
+
+
+############################
+# CREATING NEW VARIABLES
+############################
+
+df['NEW_HitRatio'] = df['Hits'] / df['AtBat']
+df['NEW_RunRatio'] = df['HmRun'] / df['Runs']
+df['NEW_CHitRatio'] = df['CHits'] / df['CAtBat']
+df['NEW_CRunRatio'] = df['CHmRun'] / df['CRuns']
+df['NEW_Avg_AtBat'] = df['CAtBat'] / df['Years']
+df['NEW_Avg_Hits'] = df['CHits'] / df['Years']
+df['NEW_Avg_HmRun'] = df['CHmRun'] / df['Years']
+df['NEW_Avg_Runs'] = df['CRuns'] / df['Years']
+df['NEW_Avg_RBI'] = df['CRBI'] / df['Years']
+df['NEW_Avg_Walks'] = df['CWalks'] / df['Years']
+
+############################
+# ENCODING
+############################
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+# ONE HOT ENCODING
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = one_hot_encoder(df, cat_cols)
+
+############################
+# MODELLING
+############################
+
+df_null = df[df["Salary"].isnull()]  # Separating the null values in the salary.
+df.dropna(inplace=True)  # Remove missing values in salary
+
+y = df['Salary']
+X = df.drop("Salary", axis=1)
+
+
+# HOLD OUT - MODEL VALIDATION
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+##########################
+# BASE MODELS
+##########################
+
+
+def all_models(X, y, test_size=0.2, random_state=12345, classification=True):
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, \
+        roc_auc_score, confusion_matrix, classification_report, plot_roc_curve, mean_squared_error
+
+    # Tum Base Modeller (Classification)
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.ensemble import GradientBoostingClassifier
+    from catboost import CatBoostClassifier
+    from lightgbm import LGBMClassifier
+    from sklearn.svm import SVC
+
+    # Tum Base Modeller (Regression)
+    from catboost import CatBoostRegressor
+    from lightgbm import LGBMRegressor
+    from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+    from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+    from sklearn.neighbors import KNeighborsRegressor
+    from sklearn.svm import SVR
+    from sklearn.tree import DecisionTreeRegressor
+    from xgboost import XGBRegressor
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=random_state)
+    all_models = []
+
+    if classification:
+        models = [('LR', LogisticRegression(random_state=random_state)),
+                  ('KNN', KNeighborsClassifier()),
+                  ('CART', DecisionTreeClassifier(random_state=random_state)),
+                  ('RF', RandomForestClassifier(random_state=random_state)),
+                  ('SVM', SVC(gamma='auto', random_state=random_state)),
+                  ('XGB', GradientBoostingClassifier(random_state=random_state)),
+                  ("LightGBM", LGBMClassifier(random_state=random_state)),
+                  ("CatBoost", CatBoostClassifier(verbose=False, random_state=random_state))]
+
+        for name, model in models:
+            model.fit(X_train, y_train)
+            y_pred_train = model.predict(X_train)
+            y_pred_test = model.predict(X_test)
+            acc_train = accuracy_score(y_train, y_pred_train)
+            acc_test = accuracy_score(y_test, y_pred_test)
+            values = dict(name=name, acc_train=acc_train, acc_test=acc_test)
+            all_models.append(values)
+
+        sort_method = False
+    else:
+        models = [('LR', LinearRegression()),
+                  ("Ridge", Ridge()),
+                  ("Lasso", Lasso()),
+                  ("ElasticNet", ElasticNet()),
+                  ('KNN', KNeighborsRegressor()),
+                  ('CART', DecisionTreeRegressor()),
+                  ('RF', RandomForestRegressor()),
+                  ('SVR', SVR()),
+                  ('GBM', GradientBoostingRegressor()),
+                  ("XGBoost", XGBRegressor()),
+                  ("LightGBM", LGBMRegressor()),
+                  ("CatBoost", CatBoostRegressor(verbose=False))]
+
+        for name, model in models:
+            model.fit(X_train, y_train)
+            y_pred_test = model.predict(X_test)
+            y_pred_train = model.predict(X_train)
+            rmse_test = np.sqrt(mean_squared_error(y_test, y_pred_test))
+            rmse_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+            values = dict(name=name, RMSE_TRAIN=rmse_train, RMSE_TEST=rmse_test)
+            all_models.append(values)
+
+        sort_method = True
+    all_models_df = pd.DataFrame(all_models)
+    all_models_df = all_models_df.sort_values(all_models_df.columns[2], ascending=sort_method)
+    print(all_models_df)
+    return all_models_df
+
+
+all_models = all_models(X, y, test_size=0.2, random_state=46, classification=False)
